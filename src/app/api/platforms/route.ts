f086f24c5eb3d057589fa7578c0c46aee1f2ea6c
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-03-20 19:40:37 +0600

    Refactor POST platform API route to enhance error handling and response structure

--- src/app/api/platforms/route.ts
+++ src/app/api/platforms/route.ts
@@ -1,16 +1,26 @@
 import { NextResponse } from "next/server";
 import { connectToMongoDB } from "@/lib/mongodb";
 import Platform from "@/models/platform.model";
+import { sendResponse } from "@/utils/server/response.handler";
+import { HTTP_STATUS_CODES } from "@/utils/server/http-status-codes";
+import { sendError } from "@/utils/server/error.handler";
 
 // Create a new platform (POST)
 export async function POST(req: Request) {
   await connectToMongoDB();
 
   try {
-    const { name, paymentType, hourlyRate, nextPayDate, day, events } =
-      await req.json();
+    const body = await req.json();
+    console.log("Received payload:", body);
 
-    if (!name || !paymentType || !hourlyRate || !nextPayDate) {
+    const { name, paymentType, hourlyRate, nextPayDate, day, events } = body;
+
+    if (!name || !paymentType || !hourlyRate) {
+      console.error("Missing required fields:", {
+        name,
+        paymentType,
+        hourlyRate,
+      });
       return NextResponse.json(
         { error: "Missing required fields" },
         { status: 400 }
@@ -27,11 +37,16 @@ export async function POST(req: Request) {
     });
     await newPlatform.save();
 
-    return NextResponse.json(newPlatform, { status: 201 });
+    return sendResponse(
+      newPlatform,
+      HTTP_STATUS_CODES.CREATED,
+      "Platform created successfully"
+    );
   } catch (error) {
-    return NextResponse.json(
-      { error: "Internal Server Error", details: error },
-      { status: 500 }
+    return sendError(
+      HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR,
+      "Error",
+      error instanceof Error ? error.message : "Unknown error"
     );
   }
 }
