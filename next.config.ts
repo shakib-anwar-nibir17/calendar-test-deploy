6cf5be2307d414480bb23946e2184f753226a156
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-03-22 00:20:16 +0600

    Update Next.js configuration to nest TypeScript settings; enable ignoreBuildErrors within typescript object

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
